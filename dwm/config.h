7851745d19a411c3c972ca71294a7be744cd5fae
danemasen <contact@danemasen.com>
2024-09-25 08:52:54 +0200

    Edit bookmarks path.

--- dwm/config.h
+++ dwm/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,7 +99,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_x,                      7)
 	TAGKEYS(                        XK_c,                      8)
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
-	{ MODKEY,			XK_Insert, spawn,          SHCMD("xdotool type $(grep -v '^#' ~/Documents/bookmarks | dmenu -i -l 50 | cut -d' ' -f1)") },
+	{ MODKEY,			XK_Insert, spawn,          SHCMD("xdotool type $(grep -v '^#' ~/Documents/bookmarks/bookmarks | dmenu -i -l 50 | cut -d' ' -f1)") },
 };
 
 /* button definitions */
