9c4801cab01f08b89258f71370fd7221dc73fa9e
danemasen <contact@danemasen.com>
2024-10-06 11:15:35 +0200

    Remove dmenu patch: highlight.

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 800;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -11,15 +11,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-		/*     fg         bg       */
+	/*     fg         bg       */
 	[SchemeNorm] = { "#d3c6aa", "#272e33" },
 	[SchemeSel] = { "#272e33", "#a7c080" },
-	[SchemeSelHighlight] = { "#a7c080", "#3c4841" },
-	[SchemeNormHighlight] = { "#d3c6aa", "#3c4841" },
 	[SchemeOut] = { "#e67e80", "#493b40" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 16;
+static unsigned int lines      = 8;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -28,4 +26,4 @@ static unsigned int lines      = 16;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 0;
